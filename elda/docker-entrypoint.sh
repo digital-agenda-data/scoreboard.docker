d901c45302deaa1b60b592da5479864e4ddbf8b7
valentinab25 <valentinab25>
2018-05-24 19:05:15 +0300

    Elda fix, docker refactor

--- elda/docker-entrypoint.sh
+++ elda/docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 cd $ELDA_HOME
 
+if [ -n "$ELDA_BASE_URL" ] && [ $(grep -c ELDA_BASE_URL /var/local/elda/webapps/elda/specs/scoreboard.ttl) -gt 0 ]; then
+    
+    sed -i "s/ELDA_BASE_URL/$ELDA_BASE_URL/g" /var/local/elda/webapps/elda/specs/scoreboard.ttl 
+
+
+fi
+
+
 exec $@
 
 
