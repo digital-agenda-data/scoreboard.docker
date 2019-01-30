d63220e439e44c248cbc663740423098afa62226
Andrei Melis <andrei.melis@eaudeweb.ro>
2019-01-30 16:10:39 +0200

    Access permissions and volumes for test/prod migration_packages folder

--- docker/elda/docker-entrypoint.sh
+++ docker/elda/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd $ELDA_HOME
 
-if [ -n "$SPARQL_ENDPOINT" ] && [ $(grep -c ELDA_SPARQL_ENDPOINT /var/local/elda/webapps/elda/specs/scoreboard.ttl) -gt 0 ]; then
+if [ -n "$SPARQL_ENDPOINT" ] && [ $(grep -c SPARQL_ENDPOINT /var/local/elda/webapps/elda/specs/scoreboard.ttl) -gt 0 ]; then
     sed -i "s#SPARQL_ENDPOINT#$SPARQL_ENDPOINT#g" /var/local/elda/webapps/elda/specs/scoreboard.ttl
 fi
 
